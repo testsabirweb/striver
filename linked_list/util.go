133f4fb416e75098f6e6c7a15231bbdcaa152a62
testsabirweb <53115363+testsabirweb@users.noreply.github.com>
2022-10-12 14:23:12 +0530

    Create util.go

---
+++ linked_list/util.go
@@ -0,0 +1,73 @@
+package linkedlist
+
+// Delete given node in a Linked List : O(1) approach
+// Problem Statement: Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list instead, you will be given access to the node to be deleted directly. It is guaranteed that the node to be deleted is not a tail node in the list.
+import "fmt"
+
+type Node struct {
+	prev *Node
+	next *Node
+	key  interface{}
+}
+
+type List struct {
+	head *Node
+	tail *Node
+}
+
+func (L *List) Insert(key interface{}) {
+	list := &Node{
+		next: L.head,
+		key:  key,
+	}
+	if L.head != nil {
+		L.head.prev = list
+	}
+	L.head = list
+
+	l := L.head
+	for l.next != nil {
+		l = l.next
+	}
+	L.tail = l
+}
+
+func (l *List) Display() {
+	list := l.head
+	for list != nil {
+		fmt.Printf("%+v ->", list.key)
+		list = list.next
+	}
+	fmt.Println()
+}
+
+func Display(list *Node) {
+	for list != nil {
+		fmt.Printf("%v ->", list.key)
+		list = list.next
+	}
+	fmt.Println()
+}
+
+func ShowBackwards(list *Node) {
+	for list != nil {
+		fmt.Printf("%v <-", list.key)
+		list = list.prev
+	}
+	fmt.Println()
+}
+
+func (l *List) Reverse() {
+	curr := l.head
+	var prev *Node
+	l.tail = l.head
+
+	for curr != nil {
+		next := curr.next
+		curr.next = prev
+		prev = curr
+		curr = next
+	}
+	l.head = prev
+	Display(l.head)
+}
